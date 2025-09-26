7661a51bcae61ec30943910c06989327ba969161
BrayanGtrR34 <brayan.batista.santos@escola.pr.gov.br>
2025-09-26 19:13:38 -0300

    Add files via upload

---
+++ Flashcard/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'One Piece',
+    'Qual é a fruta do Luffy?',
+    'É a Hito Hito no Mi, modelo Nika'
+)
+
+criaCartao(
+    'Jojo Bizarre Advnture',
+    'Qual é o protagonista mais fraco?',
+    'É o Jonathan Joestar'
+)
+
+criaCartao(
+    'Naruto',
+    'Qual é o jutsu mais proibido?',
+    'Edo Tensei' )
+
+criaCartao(
+    'Dragon Ball',
+    'Qual vilão o Goku nunca mordeu?',
+    'o Cell!!'
+)
